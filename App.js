b50b7ccbb66b9a1e1bddb4e6a7afdb8615da3b6b
Yuvraj Singh Bhadouria <yuvisingh1810@gmail.com>
2024-01-14 00:56:40 +0530

    assignment - 02, pt. 01

--- App.js
+++ App.js
@@ -1,5 +1,58 @@
-const heading = React.createElement("h1",{id: "heading", class: "head"},"Hi, this is React!");
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);  // the render method basically converts the object we get from our createElement method to the h1 tag that we desire
+// ASSIGNMENT - 02, pt. 01
+
+
+const header = React.createElement(
+    "div",
+    {className: "title"},
+    [React.createElement(
+        "h1",
+        {},
+        "Hi"
+    ),
+    React.createElement(
+        "h2",
+        {},
+        "My name is "
+    ),
+    React.createElement(
+        "h3",
+        {},
+        "Yuvraj"
+    )]
+);
+
+const headerJSX = (
+    <div className="title">
+        <h1>Hi</h1>
+        <h2>My name is</h2>
+        <h3>Yuvraj</h3>
+    </div>
+);
+
+const CompComp = () => {
+    return (
+        <h1>This is Component Composition</h1>
+    )
+}
+
+const HeaderFunComp = () => {
+    return (
+        <div className="title">
+            <h1>Hi</h1>
+            <h2>My name is</h2>
+            <h3>Yuvraj</h3>
+            <CompComp/>
+        </div>
+    );
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeaderFunComp/>);
+
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-Namaste React Course
+# Namaste React Course 🤞
+
+# Parcel
+    - Dev Build
+    - Local Server
+    - HMR = Hot Module Replacement (Automatically Refreshing our page to reflect changes)
+    - File Watching Algorithm in C++
+    - Caching - Faster Builds
+    - Image Optimization
+    - Bundle our files
+    - Compress our files
+    - Consistent Hashing
+    - Code Splitting
+    - Differential Bundling - Supports Older Browsers
+    - Diagnostic
+    - Better Error Handling
+    - Supports HTTPs
+    - Tree Shaking (removes unused code)
+    - Different dev and Production Bundles (since there are more optimization in production build than dev build)
+
